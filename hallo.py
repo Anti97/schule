07a79e5564176d337140e9d693596eea29352a57
Anti97 <niklas.rau97@gmail.com>
2025-04-04 08:56:12 +0200

    refactor: extract func tschuess()

--- hallo.py
+++ hallo.py
@@ -1,3 +1,13 @@
+def begruessung(name:str):
+    print("Hallo", name, "!")
+
+name = input("Bitte geben Sie den Namen ein: ")
+begruessung(name)
+print("Auf Wiedersehen!")
+
+def tschuess(name:str):
+    print("Auf Wiedersehen,", name, "!")
+
 name = input("Bitte geben Sie den Namen ein: ")
 print("Hallo", name, "!")
-print("Auf Wiedersehen!")
+tschuess(name)
